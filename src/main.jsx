4a24e46ffabaed6a20cf6938b4760ade44f01c01
fal24 <fal24pitt@gmail.com>
2025-06-24 13:31:50 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Detail from "./Detail"; // you’ll make this file next
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/module/:slug" element={<Detail />} />
+    </Routes>
+  </BrowserRouter>
 );
